aa3c27964cd5a83405dc587689c5530e996e7427
Will Snider <73492760+willsnider927@users.noreply.github.com>
2021-03-17 17:22:05 -0600

    Delete ECEN2310/midterm2_snider_will directory

--- ECEN2310/midterm2_snider_will/mechanism.m
+++
@@ -1,7 +0,0 @@
-function e = mechanism(th,x,L)
-    e = [L(1,:)*cos(th)+L(2,:)*cos(x(1,:))-L(3,:)*cos(x(2,:))-L(4,:);
-        L(1,:)*sin(th)+L(2,:)*sin(x(1,:))-L(3,:)*sin(x(2,:));
-        L(3,:)*cos(x(2,:))+L(5,:)*cos(x(3,:))-L(6,:)*cos(x(4,:))-L(7,:);
-        L(3,:)*sin(x(2,:))+L(5,:)*sin(x(3,:))-L(6,:)*sin(x(4,:))];
-end
-
